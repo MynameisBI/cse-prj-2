890b9cb78e317b7a8c8c01bbc97470391ed7bb61
MynameisBI <56908557+MynameisBI@users.noreply.github.com>
2022-10-30 19:25:01 +0700

    Version 1
    - Match template using Sum of Absolute Difference
    - Add functions for loading images, draw rectangles and write images

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 #include "./headers/stb_image_write.h"
 
 
-const int max_path_length = 32;
+#define MAX_PATH_LENGTH 36
 
 
 unsigned char* uc_arrayNew_1d(int size) {
@@ -18,16 +18,16 @@ unsigned char* uc_arrayNew_1d(int size) {
 
 unsigned char* load_frames(const char* path_template, int num, int* w, int* h, int* channel) {
     // get frame dimensions to initialize array frames 
-    char first_frame_path[max_path_length];
-    snprintf(first_frame_path, max_path_length * sizeof(char), path_template, 0);
+    char first_frame_path[MAX_PATH_LENGTH];
+    snprintf(first_frame_path, MAX_PATH_LENGTH * sizeof(char), path_template, 0);
     stbi_load(first_frame_path, w, h, channel, 0);
 
     // load array frames
     unsigned char* frames = uc_arrayNew_1d(num * *w * *h * *channel);
     for (int i = 0; i < num; i++) {
         // get frame path
-        char frame_path[max_path_length];
-        snprintf(frame_path, max_path_length * sizeof(char), path_template, i);
+        char frame_path[MAX_PATH_LENGTH];
+        snprintf(frame_path, MAX_PATH_LENGTH * sizeof(char), path_template, i);
 
         // load frame to array frames
         unsigned char* frame = stbi_load(frame_path, w, h, channel, 0);
@@ -53,7 +53,7 @@ unsigned int get_pixels_abs_diff(unsigned char* p1, unsigned char* p2, int chann
 }
 
 int* track_object(unsigned char* base_template, int temp_w, int temp_h,
-        unsigned char* frames, int frames_num, int frame_w, int frame_h, int channel) {
+        unsigned char* frames, int frames_num, int frame_w, int frame_h, int channel, int step) {
     int* object_positions = (int*)calloc(frames_num * 2, sizeof(int));
 
     unsigned char* template = uc_arrayNew_1d(temp_w * temp_h * channel);
@@ -66,19 +66,19 @@ int* track_object(unsigned char* base_template, int temp_w, int temp_h,
         }
     }
 
-    frames_num = 1;
     for (int i = 0; i < frames_num; i++) {
+        unsigned int frame_index = i * frame_w * frame_h * channel;
         unsigned int minSAD = -1;
         int object_position[2];
 
-        for (int frame_y = 0; frame_y < frame_h - temp_h; frame_y++) {
-            for (int frame_x = 0; frame_x < frame_w - temp_w; frame_x++) {
+        for (int frame_y = 0; frame_y < frame_h - temp_h; frame_y = frame_y + step) {
+            for (int frame_x = 0; frame_x < frame_w - temp_w; frame_x = frame_x + step) {
                 unsigned int SAD = 0;
-                for (int temp_y = 0; temp_y < temp_h; temp_y++) {
-                    for (int temp_x = 0; temp_x < temp_w; temp_x++) {
+                for (int temp_y = 0; temp_y < temp_h; temp_y = temp_y + step) {
+                    for (int temp_x = 0; temp_x < temp_w; temp_x = temp_x + step) {
                         int t_pixel_index = (temp_y * temp_w + temp_x) * channel;
-                        int f_pixel_index = (frame_y * frame_w + frame_x) * channel + t_pixel_index;
-                        SAD += get_pixels_abs_diff(&template[t_pixel_index], &frames[f_pixel_index], 3);
+                        int f_pixel_index = ((frame_y + temp_y) * frame_w + frame_x + temp_x) * channel;
+                        SAD += get_pixels_abs_diff(&template[t_pixel_index], &frames[frame_index + f_pixel_index], 3);
                     }
                 }
                 
@@ -90,65 +90,81 @@ int* track_object(unsigned char* base_template, int temp_w, int temp_h,
             }
         }
 
-        object_positions[frames_num * i] = object_position[0];
-        object_positions[frames_num * i + 1] = object_position[1];
+        object_positions[2 * i] = object_position[0];
+        object_positions[2 * i + 1] = object_position[1];
+
+        for (int y = 0; y < temp_h; y++) {
+            for (int x = 0; x < temp_w; x++) {
+                int t_pixel_index = (y * temp_w + x) * channel;
+                int f_pixel_index = ((object_position[1] + y) * frame_w + object_position[0] + x) * channel;
+                for (int ch = 0; ch < channel; ch++) {
+                    template[t_pixel_index + ch] = frames[frame_index + f_pixel_index + ch];
+                }
+            }
+        }
+
+        printf("\r%d/%d frames tracked.", i+1, frames_num);
+        fflush(stdout);
     }
+    printf("\n");
 
     return object_positions;
 }
 
 
 
-void draw_rectangles(unsigned char* images, int images_num, int* positions, int width, int height, int line_width) {
-    //debug
-    int rect_x = positions[0];
-    int rect_y = positions[1];
+void draw_rectangles(unsigned char* frames, int frames_num, int* positions, int width, int height, int channel, int line_width) {
     int rect_w = width;
     int rect_h = height;
     int lw = line_width-1;
 
-    for (int y = 0; y < 480; y++) {
-        for (int x = 0; x < 640; x++) {
-            if ((((rect_x - lw <= x && x <= rect_x) || (rect_x + rect_w <= x && x <= rect_x + rect_w + lw)) && (rect_y <= y && y <= rect_y + rect_h)) ||
-                    ((rect_y - lw <= y && y <= rect_y) || (rect_y + rect_h <= y && y <= rect_y + rect_h + lw)) && (rect_x <= x && x <= rect_x + rect_w)) {
-                int frame_i = (y * 640 + x) * 3;
-                images[frame_i] = 0;
-                images[frame_i+1] = 0;
-                images[frame_i+2] = 255;
+    for (int i = 0; i < frames_num; i++) {
+        int frame_index = 640 * 480 * channel * i;
+
+        int rect_x = positions[2 * i];
+        int rect_y = positions[2 * i + 1];
+
+        for (int y = 0; y < 480; y++) {
+            for (int x = 0; x < 640; x++) {
+                if ((((rect_x - lw <= x && x <= rect_x) || (rect_x + rect_w <= x && x <= rect_x + rect_w + lw)) && (rect_y <= y && y <= rect_y + rect_h)) ||
+                        ((rect_y - lw <= y && y <= rect_y) || (rect_y + rect_h <= y && y <= rect_y + rect_h + lw)) && (rect_x <= x && x <= rect_x + rect_w)) {
+                    int pixel_i = (y * 640 + x) * 3;
+                    frames[frame_index + pixel_i] = 0;
+                    frames[frame_index + pixel_i + 1] = 0;
+                    frames[frame_index + pixel_i + 2] = 255;
+                }
             }
         }
+    }
+}
+
+void write_frames(const char* path_template, int num, int w, int h, int channel, unsigned char* frames) {
+    for (int i = 0; i < num; i++) {
+        char frame_path[MAX_PATH_LENGTH];
+        snprintf(frame_path, MAX_PATH_LENGTH * sizeof(char), path_template, i);
+
+        int frame_index = (w * h * channel) * i;
+        stbi_write_jpg(frame_path, w, h, channel, &frames[frame_index], 100);
     }
 }
 
 
 int main() {
     int frame_width, frame_height, template_width, template_height, channel;
-    char template_path[] = "";
-
 
     unsigned char* frames = load_frames("images/frames/img%d.jpg", 63, &frame_width, &frame_height, &channel);
     unsigned char* template = stbi_load("images/template.jpg", &template_width, &template_height, &channel, 0);
+    printf("Load frames and template successfully. Tracking template.\n");
+
     int* object_positions = track_object(template, template_width, template_height, 
-            frames, 63, frame_width, frame_height, channel);
+            frames, 63, frame_width, frame_height, channel, 2);
+    printf("Track template completed. Draw rectangles.\n");
 
-    unsigned char* frame = uc_arrayNew_1d(frame_width * frame_height * 3);
-    for (int y = 0; y < frame_height; y++) {
-        for (int x = 0; x < frame_width; x++) {
-            for (int ch = 0; ch < channel; ch++) {
-                int frame_i = (y * frame_width + x) * channel + ch;
-                // frame[frame_i] = frames[4 * frame_width * frame_height * channel + frame_i];
-                frame[frame_i] = frames[frame_i];
-            }
-        }
-    }
-    draw_rectangles(frame, 1, object_positions, template_width, template_height, 2);
-    stbi_write_jpg("images/tracked_frames/sup.jpg", frame_width, frame_height, channel, frame, 100);
+    draw_rectangles(frames, 63, object_positions, template_width, template_height, channel, 2);
+    printf("Rectangles drawn. Writing down images.\n");
 
-    //     //more debugging
-    // unsigned char ps1[] = {3, 3, 3, 3};
-    // unsigned char ps2[] = {4, 3, 3, 0};
+    write_frames("images/tracked_frames/img%d.jpg", 63, frame_width, frame_height, channel, frames);
+    printf("Object tracking completed. Please check result.");
 
-    // unsigned int a = -1;
-    // unsigned int b = 1;
-    // if (a > b) printf("hello");
+    return 0;
 }
